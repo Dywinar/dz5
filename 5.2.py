0834adb2a1d6bcd8bdc8fd6fc0a718987450fd90
Dywinar <144463363+Dywinar@users.noreply.github.com>
2023-10-20 16:11:54 +0300

    Update 5.2.py

--- 5.2.py
+++ 5.2.py
@@ -1,1 +1,18 @@
--
+text = input("Сонин текст: ")
+upper_text = ''
+text_next = True # начинаем с большой буквы
+for i in text:
+  if i.isalpha(): #ЕСЛИ БУКВА
+    if text_next:
+      upper_text += i.upper()
+      text_next = False
+    else:
+      upper_text += i
+  elif i in "!?.": # ЕСЛИ !.?
+    upper_text += i
+    text_next = True # означает что надо сделать следующую букву большую
+  else: # все остальное
+    upper_text += i
+print(f"Исправленный текст: {upper_text} ")
+# капец задача 2 дня решал.... и то пришлось использовать что не проходили isalpha 
+# если что заметки это чтобы про это задачу не забывать очень интересная, чтобы если что можно было понять как она решается
